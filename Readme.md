07a05026258f5759e1b305544ac7f6bfccc988d7
Ash-ku-thakur <bhavesh10683@gmail.com>
2024-07-14 14:51:14 +0530

    login a user

--- Readme.md
+++ Readme.md
@@ -7,25 +7,43 @@ mongoose for database
 nodemon for hot-reloding
 socket.io for real-time communication connection
 
-
 userSchema for create user
 massageSchema for create massage
 conversationSchema for kiskis ke bich , aur kiakia baat
 
 
-# Array jisme jisjis ke bich baat hogi uski Ids
- participants: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-    },
-  ],
 
+# Array jisme jisjis ke bich baat hogi uski Ids
+
+participants: [
+{
+type: mongoose.Schema.Types.ObjectId,
+ref: "User",
+},
+],
 
 # Array jisme dono ke bich ki sare massages ki Ids
-  massages: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Massage",
-    },
-  ],
+
+massages: [
+{
+type: mongoose.Schema.Types.ObjectId,
+ref: "Massage",
+},
+],
+
+# register (create a user) with some fields
+
+name, username, password, confirmPassword, gender
+
+we can hash password with the help of bcrypt.hash method
+
+we can set cookie after create a user with the help of jwt.sign method
+
+# login a user
+password, username
+
+we can match the password with the help of bcrypt.comnfirm method
+
+we can also set a cookie with the help of jwt.sign method
+
+
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,45 +1,122 @@
 import User from "../models/userSchema.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
 
+dotenv.config({});
+
+// create a new user
 export let Register = async (req, res) => {
   try {
-    let { name, username, password, gender } = req.body;
+    let { name, username, password, confirmPassword, gender } = req.body;
 
     // basic validation
-    if (!name || !username || !password || !gender) {
+    if (!name || !username || !password || !confirmPassword || !gender) {
       return res.status(401).json({
         massage: "Al fields are required",
         success: false,
       });
     }
 
+    // password is correct or not
+    if (password != confirmPassword) {
+      return res.status(401).json({
+        massage: "password doesn't match",
+        success: false,
+      });
+    }
+
     // check user present or not
-    let findUser = await User.find({ email });
+    let findUser = await User.findOne({ username });
 
     //  if find
-    if (!findUser) {
+    if (findUser) {
       return res.status(401).json({
-        massage: "this email is already exist",
+        massage: "This username is already exist",
         success: false,
       });
     }
 
     // plain password convertef into hash formet
-
     let hashPassword = await bcrypt.hash(password, 10);
 
+    // profilephoto
+    let mailProfilePhoto = `https://avatar.iran.liara.run/public/boy?name=${name}`;
+    let femailProfilePhoto = `https://avatar.iran.liara.run/public/girl?name=${name}`;
+
+    
     // if not find
-    await User.create({
+   let userCreated = await User.create({
       name,
       username,
       password: hashPassword,
+      profilePhoto: gender === "mail" ? mailProfilePhoto : femailProfilePhoto,
       gender,
     });
 
-    return res.status(201).json({
+    // i want to send a token here
+
+   let token = await jwt.sign({userId:userCreated._id}, process.env.JWT_SECRET_KEY, {expiresIn:"1d"})
+
+
+    return res.status(201).cookie("uid", token, {httpOnly:rue, sameSite:"strict", maxAge:1*24*60*60*1000}).json({
       massage: "user created successfully",
       success: true,
     });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// login
+export let Login = async (req, res) => {
+  try {
+    let { username, password } = req.body;
+
+    // basic validation
+    if (!username || !password) {
+      return res.status(401).json({
+        massage: "All fields are required",
+        success: false,
+      });
+    }
+
+    // find user
+    let user = await User.findOne({ username });
+
+    // find or not
+    if (!user) {
+      return res.status(401).json({
+        massage: "password doesn't match",
+        success: false,
+      });
+    }
+
+    let isPasswordMatch = await bcrypt.compare(password, user.password);
+
+    // if password is not match
+    if (!isPasswordMatch) {
+      return res.status(401).json({
+        massage: "username or password is not match",
+        success: false,
+      });
+    }
+
+    // token send keryege
+    let token = await jwt.sign(
+      { userId: user._id },
+      process.env.JWT_SECRET_KEY,
+      { expiresIn: "1d" }
+    );
+
+    return res
+      .status(201)
+      .cookie("uid", token, { httpOnly: true, sameSite: "strict", secure:true, maxAge:1*24*60*60*1000 })
+      .json({
+        massage: `welcome back ${user.name}`,
+        success: true,
+        confirmUser: user,
+      });
   } catch (error) {
     console.log(error);
   }
